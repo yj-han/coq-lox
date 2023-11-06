42b2b2b396bb251c5136706cb5f31c6965e933ac
Yeji Han <yejicse@gmail.com>
2023-11-06 14:24:59 +0900

    WIP: Parser.v

--- src/lib/Parser.v
+++ src/lib/Parser.v
@@ -11,79 +11,114 @@ Require Import List.
 Require Import Token.
 Require Import Expr.
 
-Definition peek (tokens : list token) (current : nat) : token.
-Proof. Admitted.
 
-Definition prev (tokens : list token) (current : nat) : token.
-Proof. Admitted.
+Section ERROR_HANDLING.
+  (* Possible errors from scanning *)
+  Variant err_type :=
+    | syntax_err.
 
-Definition advance (tokens : list token) (current : nat) : (token * nat).
-Proof. Admitted.
+  (* Result: Ok or Err *)
+  Inductive result (X: Type): Type :=
+  | Ok (x: X)
+  | Err (e: err_type).
 
-Definition check (tokens : list token) (current : nat) (ttype : token_type) :=
-  let token := peek tokens current in
-  Token.eqb token.(type) ttype.
+  Definition error_msg (e: err_type): string :=
+    match e with
+    | syntax_err => "Syntax error"
+    end.
 
+End ERROR_HANDLING.
 
-Definition match_token_types (tk_types : list token_type) (tk_type : token_type) :=
-  existsb (Token.eqb tk_type) tk_types.
-
-
-Module ParseError.
-  
-End ParseError.
-
-Definition parse_expr_assign (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_binop (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_call (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_get (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_grouping (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_literal (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_logical (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_set (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_super (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_unop (tokens : list token) : expr.
-Proof. Admitted.
-Definition parse_expr_var (tokens : list token) : expr.
-Proof. Admitted.
+Arguments Ok {X}.
+Arguments Err {X}.
 
 
-Definition parse_stmt_for (tokens : list token) : stmt.
-Proof. Admitted.
-Definition parse_stmt_ite (tokens : list token) : stmt.
-Proof. Admitted.
-Definition parse_stmt_print (token : list token) : stmt.
-Proof. Admitted.
-Definition parse_stmt_ret (tokens : list token) : stmt.
-Proof. Admitted.
-Definition parse_stmt_while (tokens : list token) : stmt.
-Proof. Admitted.
-Definition parse_stmt_block (tokens : list token) : stmt.
-Proof. Admitted.
-Definition parse_stmt_expr (tokens : list token) : stmt.
-Proof. Admitted.
+Variant state :=
+  | glob
+  | in_class (name: token) (super: option expr) (body: list stmt)
+  | in_function (name: token) (params: list token) (body: list stmt) (is_param: bool)
+  | in_var_declaration
+  | in_for_stmt
+  | in_if_stmt
+  | in_print
+  | in_ret
+  | in_while    
+  | in_expr_stmt 
+.
 
-Fixpoint parse_stmt (tokens : list token) : stmt.
-Proof. Admitted.
+Definition concat_with_casting (X : Type) (tk : X) (tks : result (list X)) :=
+  match tks with
+  | Ok tks => Ok (tk :: tks)
+  | _ => tks
+  end.
 
-Definition parse_function (tokens : list token) : stmt.
-Proof. Admitted.
+(* TODO: remove redundancy *)
+Arguments concat_with_casting {X}.
 
-Definition parse_class (tokens : list token) : stmt.
-Proof. Admitted.
 
-Definition parse_declaration (tokens : list token) : stmt.
+(*
+Fixpoint parse_rec
+  (tokens : list token)
+  (state : state)
+  (scope : nat)
+  : result (list stmt) :=
+  match state with
+  | base =>
+      match tokens with
+      | nil => Ok nil
+      | tk_class :: tl =>
+          match tl with
+          | tk_identifier id1 :: tk_lt :: tk_identifier id2 :: tk_left_brace :: tl =>
+              let next_state := in_class id1 (Some var id2) nil in
+              parse_rec tl next_state
+          | tk_identifier id :: tk_left_brace :: tl =>
+              let next_state := in_class id None nil in
+              parse_rec tl next_state
+          | _  => Err syntax_err
+          end
+      | tk_fun :: tl =>
+          match tl with
+          | tk_identifier :: tk_left_brace :: tl =>
+              let next_state := in_function id nil nil in
+              parse_rec tl next_state
+          | _ => Err syntax_err
+          end
+      | tk_var :: tl =>
+          match tl with
+          | tk_identifier id :: tk_eq :: tl =>
+              parse_rec tl in_var_declaration
+          | _ => Err syntax_err
+          end
+      | tk_for :: tl => Err syntax_err 
+      | tk_if :: tl => Err syntax_err
+      | tk_print :: tl => Err syntax_err
+      | tk_ret :: tl => Err syntax_err
+      | tk_while :: tl => Err syntax_err 
+      | tk_left_brace :: tl => parse_rec tl in_stmt
+      | _ => Err syntax_err
+      end
+  | in_class name super body =>
+      match tokens with
+      | tk_right_brace :: tl =>
+          let stmt := class name super body in
+          let result := parse_rec tl glob in
+          concat_with_casting stmt result            
+      | tk_fun :: tl =>
+          match tl with
+          | tk_identifier :: tk_left_brace :: tl =>
+              let next_state := in_function id nil nil false in
+              parse_rec tl next_state
+          | _ => Err syntax_err
+          end
+      | _ => Err syntax_err
+      end
+  | in_function name params body =>
+      match tokens with
+      | tk_right_brace :: tl =>
+          let stmt := function name params body in
+          let result := parse_rec tl glob in
+          concat_with_casting stmt result
+*)        
+      
+Definition parse (tokens : list token) : result (list stmt).
 Proof. Admitted.
-
-Definition parse (tokens : list token) : list stmt.
-Proof. Admitted.
-
